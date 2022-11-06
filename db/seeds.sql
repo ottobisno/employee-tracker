26a06968a2186d791365b9e206bac18df08815c8
OttoBisno <ottobisno444@gmail.com>
2022-11-06 13:44:30 -0800

    Created db files and installed packages

---
+++ db/seeds.sql
@@ -0,0 +1,28 @@
+INSERT INTO department (name)
+VALUES ("Sales"),
+       ("Engineering"),
+       ("Finance"),
+       ("Legal");
+
+INSERT INTO role (title, salary)
+VALUES ("Sales Lead", 100000),
+       ("Salesperson", 80000),
+       ("Lead Engineer", 150000),
+       ("Software Engineer", 120000),
+       ("Account Manager", 160000),
+       ("Accountant", 125000),
+       ("Legal Team Lead", 250000),
+       ("Lawyer", 190000);
+
+INSERT INTO employee (first_name, last_name, )
+VALUES  ("Mark", "Jacobs"),
+        ("Bill", "Benson"),
+        ("Jake", "Roberts"),
+        ("Phillip", "Colby"),
+        ("Teran", "Marks"),
+        ("Jeremy", "Cambridge"),
+        ("Teresa", "Hearth"),
+        ("Jane", "Janely"),
+        ("Elizabeth", "Rutherford"),
+        ("Helen", "Curtis"),
+        ("Jamie", "Valentina");
