da9b339439633bae4419710d24890c4cbb932a6e
OttoBisno <ottobisno444@gmail.com>
2022-11-06 16:49:48 -0800

    Polished up database and seeds file

--- db/schema.sql
+++ db/schema.sql
@@ -4,27 +4,27 @@ CREATE DATABASE company_db;
 USE company_db;
 
 CREATE TABLE department (
-    department_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
 );
 
 CREATE TABLE role (
-    role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
     FOREIGN KEY (department_id)
-    REFERENCES department(department_id)
+    REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    employee_id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
-    manager_id INT,
+    manager_id INT NULL
     FOREIGN KEY (role_id)
-    REFERENCES role(role_id)
+    REFERENCES role(id)
 );
 
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,28 +1,28 @@
 INSERT INTO department (name)
-VALUES ("Sales"),
-       ("Engineering"),
-       ("Finance"),
-       ("Legal");
+VALUES  ("Sales"),
+        ("Engineering"),
+        ("Finance"),
+        ("Legal");
 
-INSERT INTO role (title, salary)
-VALUES ("Sales Lead", 100000),
-       ("Salesperson", 80000),
-       ("Lead Engineer", 150000),
-       ("Software Engineer", 120000),
-       ("Account Manager", 160000),
-       ("Accountant", 125000),
-       ("Legal Team Lead", 250000),
-       ("Lawyer", 190000);
+INSERT INTO role (title, salary, department_id)
+VALUES  ("Sales Lead", 100000, 1),
+        ("Salesperson", 80000, 1),
+        ("Lead Engineer", 150000, 2),
+        ("Software Engineer", 120000, 2),
+        ("Account Manager", 160000, 3),
+        ("Accountant", 125000, 3),
+        ("Legal Team Lead", 250000, 4),
+        ("Lawyer", 190000, 4);
 
-INSERT INTO employee (first_name, last_name, )
-VALUES  ("Mark", "Jacobs"),
-        ("Bill", "Benson"),
-        ("Jake", "Roberts"),
-        ("Phillip", "Colby"),
-        ("Teran", "Marks"),
-        ("Jeremy", "Cambridge"),
-        ("Teresa", "Hearth"),
-        ("Jane", "Janely"),
-        ("Elizabeth", "Rutherford"),
-        ("Helen", "Curtis"),
-        ("Jamie", "Valentina");
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ("Mark", "Jacobs", 1),
+        ("Bill", "Benson", 2, 1),
+        ("Jake", "Roberts", 3),
+        ("Phillip", "Colby", 4, 3),
+        ("Teran", "Marks", 5),
+        ("Jeremy", "Cambridge", 6, 5),
+        ("Teresa", "Hearth", 7),
+        ("Jane", "Janely", 8, 7),
+        ("Elizabeth", "Rutherford", 4, 3),
+        ("Helen", "Curtis", 2, 1),
+        ("Jamie", "Valentina", 6, 5);
